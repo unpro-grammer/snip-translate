72a30c2987e75072c2b073720c9529830695bda8
Grace <gxu660@aucklanduni.ac.nz>
2026-09-11 23:21:30 +1200

    feat: set up basic popup panel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json' with { type: 'json' }
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), crx({ manifest })],
+  build: {
+    rollupOptions: {
+      input: {
+        offscreen: 'src/offscreen/index.html',
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
 })
